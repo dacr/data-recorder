c4501c816d99c6c9ebe0492cfbaab71c0569e57a
Crosson David <crosson.david@gmail.com>
2022-11-01 09:54:46 +0100

    Enhance frontend

--- modules/frontend/src/main/scala/fr/janalyse/datarecorder/Main.scala
+++ modules/frontend/src/main/scala/fr/janalyse/datarecorder/Main.scala
@@ -1,12 +1,14 @@
 package fr.janalyse.datarecorder
 
 import scala.scalajs.js
+import scala.concurrent.Future
 import org.scalajs.dom
 import fr.janalyse.datarecorder.protocol.*
 import com.raquo.laminar.api.L.*
 import org.scalajs.dom.Event
 import sttp.tapir.client.sttp.SttpClientInterpreter
 import sttp.client3.*
+import zio.*
 
 object DataRecorderService {
   import fr.janalyse.datarecorder.protocol.DataRecorderEndPoints.*
@@ -14,28 +16,31 @@ object DataRecorderService {
   val backend = FetchBackend()
 
   def serviceStatus() =
-    SttpClientInterpreter()
-      .toRequestThrowErrors(serviceStatusEndpoint, baseUri = None)
-      .apply(())
-      .send(backend)
+    val response: Future[Response[Either[Unit,ServiceStatus]]] =
+      SttpClientInterpreter()
+        .toRequestThrowDecodeFailures(serviceStatusEndpoint, baseUri = None)
+        .apply(())
+        .send(backend)
+    //ZIO.fromFuture(response)
+    response
 }
 
 object App {
 
   val runtime = zio.Runtime.default
 
-
   def root = div(
-    h1("test")
+    h1("Test")
   )
 
   def initialize(onEvent: Event): Unit = {
     val appContainer = dom.document.querySelector("#app")
     appContainer.innerHTML = ""
+
     implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
-    println("ICI")
-    DataRecorderService.serviceStatus().map(res => println(res.body.version.toString()))
-    val _            = render(appContainer, root)
+    DataRecorderService.serviceStatus().map(res => println(res.body.map(_.version.toString())))
+
+    val _ = render(appContainer, root)
   }
 
   def main(args: Array[String]): Unit = {
